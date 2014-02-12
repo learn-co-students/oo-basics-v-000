9d8452b0e7a4a66adfe8eb2832cff7155b7e2122
Logan Hasson <logan.hasson@gmail.com>
2014-02-11 21:06:57 -0500

    Complete solution, with tutorial code

---
+++ book.rb
@@ -0,0 +1,20 @@
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
+
+  def initialize(title)
+    @title = title
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
--- shoe.rb
+++ shoe.rb
@@ -1,4 +1,19 @@
 # Make your shoe class here!
 
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  BRANDS = []
+
+  def initialize(brand)
+    @brand = brand
+    BRANDS << self.brand if !BRANDS.include?(self.brand)
+  end
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
 end
