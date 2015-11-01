9356b49d6770be49eb6b2b6e99cc8954dd486ae1
joshuabamboo <joshuaowens011@gmail.com>
2015-11-01 16:10:27 -0500

    DRY specs

--- spec/02_shoe_spec.rb
+++ spec/02_shoe_spec.rb
@@ -1,4 +1,6 @@
 describe 'Shoe' do
+  let(:shoe) { Shoe.new("Nike") }
+
   describe '::new' do
     it 'gets initialized with a brand' do
       expect{Shoe.new("Adidas")}.to_not raise_error
@@ -6,8 +8,6 @@ describe 'Shoe' do
   end
 
   describe 'properties' do
-    let(:shoe) { Shoe.new("Nike") }
-
     it 'has a brand' do
       # When must the brand be assigned for this to work?
       expect(shoe.brand).to eq("Nike")
@@ -35,8 +35,6 @@ describe 'Shoe' do
   end
 
   describe '#cobble' do
-    let(:shoe) { Shoe.new("Asics") }
-
     it 'says that the shoe has been repaired' do
       expect($stdout).to receive(:puts).with("Your shoe is as good as new!")
       shoe.cobble
