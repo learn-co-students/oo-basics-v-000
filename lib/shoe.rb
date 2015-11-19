a0d70aa2ed0ded0fdd3ff18002d9ac5b34bbc225
surla <josephsurla@gmail.com>
2015-11-19 15:26:17 -0800

    update shoe class with new cobble method

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,5 @@
 class Shoe
-  attr_accessor :color, :size, :material
+  attr_accessor :color, :size, :material, :condition
   attr_writer :condition
   attr_reader :brand
 
@@ -8,16 +8,11 @@ class Shoe
   end
 
   def cobble
-    puts "Your shoe is as good as new!"
-  end
-
-
-
-  def condition
     if @condition == "old"
       @condition = "new"
     else
       @condition
     end
+    puts "Your shoe is as good as new!"
   end
 end
