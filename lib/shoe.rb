54bb44bb51aaccb93135b948e5ed5414df2c928a
surla <josephsurla@gmail.com>
2015-11-19 15:20:34 -0800

    create book class and shoe class

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material
+  attr_writer :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
+
+
+
+  def condition
+    if @condition == "old"
+      @condition = "new"
+    else
+      @condition
+    end
+  end
+end
