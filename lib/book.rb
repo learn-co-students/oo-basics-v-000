ce172bc1e497a10da0aca4cb1a7ea66a347cdbc6
Lukeghenco <lukeghenco@gmail.com>
2015-12-15 08:12:03 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,13 @@
 class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
+  def initialize(title) # Initialize Method (Hook or Callback)
+    @title = title # setter
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
 end
