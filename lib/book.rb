d0b264506ff9973ee1335e725dfb63ae324960a6
azheng249 <andy.zheng249@gmail.com>
2016-01-05 20:22:58 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,17 @@
 class Book
+  attr_accessor :title 
+  attr_accessor :author 
+  attr_accessor :page_count 
+  attr_accessor :genre
 
+  def initialize(title = nil, author = nil, page_count = nil, genre = nil)
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 end
