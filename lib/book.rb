155a4a2d88180c619773cb0de8bae1f9a757424b
tttra <tothutra@gmail.com>
2016-01-05 02:00:50 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,16 @@
 class Book
+  def initialize (title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  attr_accessor :author, :genre, :page_count
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
 end
