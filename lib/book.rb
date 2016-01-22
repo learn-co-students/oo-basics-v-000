6e7315b0db66cd93151006c995751cc757eb6d55
jmheyd <heydjm@comcast.net>
2016-01-22 15:31:22 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,54 @@
 class Book
 
+	def initialize(title)
+		@title = title
+	end
+
+	def title
+		@title
+	end
+
+	def author=(author)
+		@author = author
+	end
+
+	def author
+		@author
+	end
+
+	def page_count=(num)
+		@page_count = num
+	end
+
+	def page_count
+		@page_count
+	end
+
+	def genre=(genre)
+		@genre = genre
+	end
+
+	def genre
+		@genre
+	end
+
+	def turn_page
+		puts "Flipping the page...wow, you read fast!"
+	end
+
 end
+
+#simplified code with attribute acessors and attribute readers
+#class Book
+	#attr_accessor :author, :page_count, :genre
+	#attr_reader :title
+
+	#def initialize(title)
+		#@title = title
+	#end
+
+	#def turn-page
+		#puts "Flipping the page...wow, you read fast!"
+	#end
+
+#end
