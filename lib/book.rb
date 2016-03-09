409457c74a69c988e0d1a2542206221f19530638
motti770 <mottigottlieb@gmail.com>
2016-03-09 01:18:27 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,20 @@
+require 'pry'
 class Book
+      attr_accessor :author, :page_count, :genre
 
+  def initialize(title)
+    @title=title
+  end
+
+  def title
+      @title
+  end
+
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 end
+
+#binding.pry
+Book.new("And Then There Were None")
