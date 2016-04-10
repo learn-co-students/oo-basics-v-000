703cd5079ae3a4c6fc51924217dbe16df3bc7a0d
karuna24s <karuna24s@gmail.com>
2016-04-10 10:20:53 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,10 @@
 # Make your shoe class here!
 class Shoe
   attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
   def initialize(brand)
     @brand = brand
-  end
-
-  def brand
-    @brand
   end
 
   def cobble
