a368ef7c3469d5e79746d05e8a1819338ddbd557
m-walker <michaelwalker610@gmail.com>
2016-04-19 14:09:02 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,38 @@
 class Book
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def author=(name)
+    @author = name
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(pages)
+    @page_count = pages
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
 end
