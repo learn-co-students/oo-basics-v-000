0b150e9decfd24edd3e7812a6c459657e30e6d84
jd2rogers2 <jd2rogers2@gmail.com>
2016-04-28 12:09:22 -0400

    donzo

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,18 @@
 class Book
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  attr_accessor :genre, :page_count, :author
+
+  attr_reader :title
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
 end
