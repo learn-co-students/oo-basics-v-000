b026bf19a776261095ddeda879d5a7b8792afaff
SnowboardTechie <bryan.thompson518@gmail.com>
2016-05-22 08:19:34 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,13 @@
 class Book
+  attr_accessor :page_count, :genre, :author
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
 end
