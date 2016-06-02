c105d51a84264b8cb32e1383f98ba33fcf1b3175
Emmanuel Owusu <e_owusu@live.com>
2016-06-01 21:49:57 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,4 +2,16 @@
 
 class Shoe
 
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble()
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+
+
 end
