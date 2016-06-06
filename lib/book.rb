d765a90da6f3011fb4a9d8e2e3ca5bd5c6802412
Certain125 <amanita.certain@gmail.com>
2016-06-06 17:39:59 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,40 @@
 class Book
 
+  def initialize(title)
+    @title = title
+  end
+
+  def title #getter method
+    @title
+  end
+
+  def author=(author) #setter method, has XYZ= 
+    @author = author
+  end
+  #setters and getters mostly come in pairs so now we need
+  #an author method
+  def author
+    @author
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+
+class Shoe
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
+
