0aeaea0cd62d3a6d7fd7cc067e97dfdf7c5b8da6
Nicholas-Maxwell <nick.maxwell18@gmail.com>
2016-06-17 13:29:58 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,76 @@
+require 'pry'
 class Book
 
+  def initialize(title)   
+    @title = title        
+  end 
+
+  def title   # "GETTER" 'gets' a property for Us. 
+  @title  
+  end 
+
+  def author=(author) # "SETTER" setting a  property or instance variable. 'property='
+    @author = author 
+  end 
+  #if we leave the setter above we get a no method error for author. SETTERS & GETTERS COME IN PAIRS 
+  #Therefore, we must add the "GETTER" for AUTHOR 2 our Book below. 
+
+  def author 
+    @author  #--> GETTER TO GO WITH OUR SETTER
+  end  
+#error undefined method page_count. WE MUST APPLY SETTER & GETTER TO  GIVE A PAGE COUNT Below 
+
+def page_count=(num)
+  @page_count = num
+end 
+
+def page_count
+  @page_count
+end 
+
+def genre=(genre)
+  @genre = genre
+end 
+
+def genre
+  @genre 
+end 
+
+#GIVING OUR CLASSES BEHAVIOR 
+#HOW? BY GIVING THEM METHODS THAT DO STUFF! I.E. 'book can turn pages' --> turn_pages
+
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end 
+
+#or book = Book.new("Some Title")
+#book.turn_page 
+
+#binding.pry
 end
+
+
+
+#class Book
+ # attr_accessor :author, :page_count, :genre
+  #attr_reader :title
+ 
+  #def initialize(title)
+   # @title = title
+  #end
+ 
+  #def turn_page
+   # puts "Flipping the page...wow, you read fast!"
+  #end
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe 
+
+attr_accessor :color, :size, :material, :condition
+attr_reader :brand 
+
+ def initialize(brand)
+   @brand = brand 
+end 
+
+#cobble says that the shoes has been repairs and makes the shoe's condition new
+def cobble 
+  puts "Your shoe is as good as new!"
+  @condition = "new"
+end 
+ 
+end 
