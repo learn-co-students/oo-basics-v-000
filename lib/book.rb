1d67ce2dd5f8b8b1ade05f2ef50b72d8d347021d
Bryna Dunnells <badunnells@gmail.com>
2016-07-12 02:24:03 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -5,28 +5,7 @@ end
 def title
   @title
 end
-
-def author=(author)
-  @author = author
-end
-def author
-  @author
-end
-
-def page_count=(page_count)
-  @page_count = page_count
-end
-def page_count
-  @page_count
-end
-
-def genre=(genre)
-  @genre = genre
-end
-def genre
-  @genre
-end
-
+attr_accessor :page_count, :genre, :author
 def turn_page
   puts "Flipping the page...wow, you read fast!"
 end
