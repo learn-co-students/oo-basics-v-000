0c7725e8891bf38e2ad7ce3033bbe4e65966567b
Binny Lewis <binnylewis@gmail.com>
2016-08-12 12:07:10 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
 
+class Book
+  def initialize(title)
+    @title = title
+    @page_count = 0
+  end
 
+  # using macros to create author,genre,title getter and setter
+  attr_accessor :author
+  attr_accessor :genre
+  attr_accessor :title
+  attr_accessor :page_count
+
+  # add 1 to page count
+  def turn_page
+    @page_count += 1
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+    @color = brand
+    @size = brand
+    @material = brand
+    @condition = "tattered"
+  end
+  attr_accessor :brand
+  attr_accessor :color
+  attr_accessor :size
+  attr_accessor :material
+  attr_accessor :condition
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
