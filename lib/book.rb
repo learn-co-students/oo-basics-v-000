ff93c94813394326ea66c3e479bdb2ed8094e9a7
smithWEBtek <brad@smithwebtek.com>
2016-09-05 18:29:20 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,6 @@
-
 class Book
   attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_reader :title 
 
   def initialize(title)
     @title = title
@@ -14,69 +13,4 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Book
-
-#   def initialize(title)
-#     @title = title
-#   end
-
-#   def title
-#     @title
-#   end
-
-#   def author=(author)
-#     @author = author
-#   end
-
-#   def author
-#     @author 
-#   end
-
-#   def page_count=(num)
-#     @page_count = num
-#   end
-
-#   def page_count
-#     @page_count
-#   end
-
-#   def genre=(genre)
-#     @genre = genre
-#   end
-
-#   def genre
-#     @genre
-#   end
-
-#   def turn_page
-#     puts "Flipping the page...wow, you read fast!"
-#   end
-
-
-# end
-
-book = Book.new("How To Change A Light Bulb")
-book.turn_page
-
-
-
