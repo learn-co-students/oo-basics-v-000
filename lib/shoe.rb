cf45ea03c21046e416d7c333f7d359ff02c469d0
Daniel Silverman <daniel.silverman@me.com>
2016-12-24 21:32:33 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,54 @@
 # Make your shoe class here!
+class Shoe
+
+  def initialize(brand)
+      @brand = brand
+    end
+
+  def brand
+    @brand
+  end
+
+  def color=(color)
+    @color = color
+  end
+
+  def color
+    @color
+  end
+
+  def size=(size)
+    @size = size
+  end
+
+  def size
+    @size
+  end
+
+  def material=(material)
+  @material = material
+end
+
+def material
+  @material
+end
+
+def condition=(condition)
+  @condition = condition
+end
+
+def condition
+  @condition
+end
+
+def cobble
+  @condition = "new"
+  puts "Your shoe is as good as new!"
+end
+
+end
+
+# refactoring
+# class Shoe
+# attr_accessor :color, :size, :material, :condition
+# attr_reader :brand
