27731248dfabba0a1cc15635b1040e86b9d773e4
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-12-31 12:59:27 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+class Book
+  def initialize(title)
+    @title=title
+  end
 
+  def title
+    @title
+  end
 
+  def author=(author)
+    @author=author
+  end
+
+  def author
+    @author
+  end
+  
+
+end
