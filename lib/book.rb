496dd983e3306c3786d7e81d1a8a87bac95e7685
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-12-31 13:05:19 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -14,6 +14,21 @@ class Book
   def author
     @author
   end
-  
+
+  def page_count=(num)
+    @page_count=num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre=genre
+  end
+
+  def genre
+    @genre
+  end
 
 end
