c36e1b9df1e847a16097fc117c0fe2bc97c9d830
Tom Su <djtomblue@gmail.com>
2017-01-02 08:49:46 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
-# Make your shoe class here!
+# Make your shoe class here!class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
