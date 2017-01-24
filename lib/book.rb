dd999ea8199a26fd32183aef0908eb87ead935d5
Kevin Adkins <kevinladkins@gmail.com>
2017-01-24 18:26:03 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -3,7 +3,9 @@ class Book
     @title = title
   end
 
-  attr_reader :title, :author, :page_count, :genre
+  attr_reader :title
+
+  attr_accessor :author, :page_count, :genre
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
