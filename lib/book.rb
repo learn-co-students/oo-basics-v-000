fb1dbb51ffb3c42f31aaae5b8ab4fb6767431732
Kevin Adkins <kevinladkins@gmail.com>
2017-01-24 18:16:32 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+  attr_reader :title
 
+  attr_accessor :author
+
+  attr_accessor :page_count
+
+  attr_accessor :genre
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
