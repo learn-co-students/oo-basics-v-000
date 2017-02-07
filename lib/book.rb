60791bff9590631296d11e517fc4c453af189e9a
Sophia Varella <sophiavarella@gmail.com>
2017-02-07 03:43:19 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+  attr_reader :title
+  attr_accessor :author, :page_count, :genre
 
+  def initialize(title)
+    @title = title
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
