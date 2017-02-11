2a1db928692c4ec852bf37058ae50c9fbd9acfb3
Daniel Lempesis <danlrcn@hotmail.com>
2017-02-11 03:20:19 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,44 @@
+class Book
+   #attr_accessor :title
+  def initialize(title) #title = input
+    @title = title      #@title setting class/global to title input?
+  end
 
+  def title
+    @title
+  end
 
+  def author=(author) #setter sets an instance variable. whatever the fuck that means :/
+    @author = author
+  end
+
+  def author #getter
+    @author
+  end
+
+  def page_count=(page_count)
+    @page_count = page_count
+end
+
+  def page_count
+    @page_count
+  end
+
+  def turn_page=(turn_page)
+    @turn_page = turn_page
+end
+
+  def turn_page
+    @turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre) #setter sets an instance variable. whatever the fuck that means :/
+    @genre = genre
+  end
+
+  def genre #getter
+    @genre
+  end
+
+end
