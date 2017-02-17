2a99c23b29085c8230bf5e94311df7037e572e57
Kelly Kashuda <klkashuda@crimson.ua.edu>
2017-02-17 05:44:28 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,37 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+  def title
+    @title
+  end
 
+  def author=(name)
+    @author = name
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(count)
+    @count = count
+  end
+
+  def page_count
+    @count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+end
