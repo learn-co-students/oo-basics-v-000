191ed269fd3b1ab5967b56aea6e018cebceddb18
Holger Paas <holger.paas@gmail.com>
2017-02-26 21:31:17 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
-
-
+class Book
+  def initialize(title)
+    @title = title
+  end
+  attr_accessor :title
+  attr_accessor :author
+  attr_accessor :genre
+  attr_accessor :page_count
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
