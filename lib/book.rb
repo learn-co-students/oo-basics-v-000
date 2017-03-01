a76b88e4bf725c3853839028c4d5d4c9e278b91f
Isis Tejeda <isis@isistejeda.com>
2017-03-01 13:11:41 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,38 @@
+class Book
 
+  def initialize(title) #setter method for title
+    @title = title
+  end
 
+  def title  #reader
+    @title
+  end
+
+  def author=(author) #setter method for author
+    @author = author
+  end
+
+  def author #getter for author
+    @author
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
