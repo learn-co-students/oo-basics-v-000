dec26e0453ff2c55084aad1665f8216e3fb17a0a
Seth Alexander <seth.a.alexander@gmail.com>
2017-03-11 16:27:10 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,46 @@
+class Book
 
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
+  def initialize(title)
+    @title = title
+  end
+
+  # attr_reader means we don't need this code:
+  #
+  # def title
+  #   @title
+  # end
+
+  # attr_accessor means we don't need all this code:
+  #
+  # def author=(author)
+  #   @author = author
+  # end
+  #
+  # def author
+  #   @author
+  # end
+  #
+  # def page_count=(num)
+  #   @page_count = num
+  # end
+  #
+  # def page_count
+  #   @page_count
+  # end
+  #
+  # def genre=(genre)
+  #   @genre = genre
+  # end
+  #
+  # def genre
+  #   @genre
+  # end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
