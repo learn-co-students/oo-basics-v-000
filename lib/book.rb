ef055911a382e0d1c877296a9652687070ef6154
Richard Burd <richardburdx@yahoo.com>
2017-04-03 20:08:47 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,32 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+  def author=(author)
+    @author = author
+  end
 
+  def author
+    @author
+  end
+
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+
+  def page_count
+    @page_count
+  end
+
+  attr_accessor :genre
+
+  def title
+    @title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
