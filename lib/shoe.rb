26d2e3b05ddbef979b493a6a805d7b57a6fa0a68
Leonardo Guzman <lguzman@me.com>
2017-04-04 15:51:38 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
-# Make your shoe class here!
+class Shoe
+  #becuase we are getting and setting info on the floowing properties we use attr_accessor.
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+
+  def cobble
+    #call the instance of condition to change the condition to new!
+  @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
+end
