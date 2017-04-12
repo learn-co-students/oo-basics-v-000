6584f00594394cf4df78bdb1d3bf0b32ee02a792
David Mars <david.w.mars@my.occc.edu>
2017-04-12 06:05:34 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+#   ~~~HAVICK WAS HERE ~~~
+
+class Shoe
+
+    attr_accessor :brand, :color, :size, :material, :condition
+  def initialize(brand)
+    @brand = brand
+  end
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
