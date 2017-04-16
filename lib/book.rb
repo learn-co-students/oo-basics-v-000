c59da8df93a3493db6f1f25f03a3e644602da635
sarjumulmi <mulmi.sarju@gmail.com>
2017-04-16 17:54:00 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,9 @@
-
-
+class Book
+  def initialize(title="And Then There Were None")
+    @title=title
+  end
+  attr_accessor :title, :author, :page_count, :genre
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
