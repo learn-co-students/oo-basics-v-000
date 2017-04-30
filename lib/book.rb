ff81156aee5c7d9e9bd2b737fe973d8bf1be0082
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-04-30 16:26:54 +0000

    Lesson complete."

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,46 @@
 
-
+class Book
+   #attr_accessor gives us a getter  and setter replacing the commented out code below
+   attr_accessor :author, :page_count, :genre
+   attr_reader :title
+   
+   def initialize(title)
+       @title = title
+   end
+   
+   # def title
+   #     @title
+   # end
+   
+#    def author=(author)
+#       @author = author
+#    end   
+   
+#    def author
+#       @author
+#    end
+   
+#    def page_count=(num)
+#       @page_count=(num)
+#    end
+   
+#    def page_count
+#       @page_count
+#    end
+   
+#    def genre=(genre)
+#     @genre = genre
+#    end
+ 
+#  def genre
+#     @genre
+#  end
+  
+  def turn_page
+     #this method makes the book turn the page. 
+     #we can give our class behaviours as sell as properties
+     puts "Flipping the page...wow, you read fast!"
+  end
+   
+   
+end
