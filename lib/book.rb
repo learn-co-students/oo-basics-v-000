2d17c3612cc1ae59fbe8148176e740776265f179
Jacilyn Hayden <jacihayden@gmail.com>
2017-05-04 01:39:44 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+  attr_accessor :title, :author, :page_count, :genre
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
