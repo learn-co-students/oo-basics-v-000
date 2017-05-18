e0e3bd4b775f9bf82828c78126308a85afda74e7
Noah Summers <summers.noah@gmail.com>
2017-05-18 05:37:45 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre
 
+  def initialize(input)
+    @title = input
+    
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(input)
+    @brand = input
+    
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
+end
