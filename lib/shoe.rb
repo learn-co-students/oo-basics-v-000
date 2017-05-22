8b9dd93c23339735d96e721b27ab3f3444f417f0
Samson Yuwono <samson.yuwono@gmail.com>
2017-05-22 15:02:19 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,10 @@
 
 class Shoe
   attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
   def initialize(brand)
     @brand = brand
-  end
-
-  def brand
-    @brand
   end
 
   def cobble
