ca3207f5524e8f9d580cc8bf0105099460f662de
Loryleen Batioco <loryleenbatioco@gmail.com>
2017-07-28 23:01:49 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+  def initialize(book_title)
+    @title = book_title
+  end
+  attr_reader :title
 
+  attr_accessor :author, :page_count, :genre
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
