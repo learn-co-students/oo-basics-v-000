0b9ae8635ad54951e81d0f69bd8c0956c0467983
Mars Howard <marsbhoward@gmail.com>
2017-08-02 00:50:32 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+class Book
+  attr_accessor :title, :author, :genre
 
+  def initialize(title)
+      @title = title
+  end
 
+  def page_count= (num)
+    @page_count = num
+  end
+
+  def page_count
+   @page_count
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
