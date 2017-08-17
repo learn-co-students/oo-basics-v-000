ce82c3153de8f025c44a5530ec6fa7beb0198abf
Archana Burra <burra.archana@gmail.com>
2017-08-17 02:26:52 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,11 @@
 
 
 class Book
-  attr_accessor :title, :author, :page_count, :genre
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
-  def initialize(name)
-    @title = name
+  def initialize(title)
+    @title = title
   end
 
   def turn_page
