833ef6bd83b1a8ba6b7eff240d063dc765fc3455
ronald vilorio <soccer23415@gmail.com>
2017-08-31 18:40:40 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,36 @@
+class Book
+  def initialize(title)
+    @title = title
 
+  end
+  # getter
+  def title
+    @title
+  end
 
+  # setter
+  def author=(author)
+    @author = author
+  end
+  def author
+    @author
+  end
+
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+  def page_count
+    @page_count
+  end
+  def genre=(genre)
+    @genre = genre
+  end
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
