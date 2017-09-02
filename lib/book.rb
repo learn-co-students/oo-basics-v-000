b2226adb5df5b8975e66cd2a8c215e66a543f63c
Robert Laws <roblaws@me.com>
2017-09-02 15:01:53 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+  attr_reader :title
+  attr_accessor :author, :page_count, :genre
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
