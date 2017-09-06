2c4f0ed3d1202b41b4cb5e62ad8468da29a16abb
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-06 04:33:42 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
 
 
+end
+
+
+# learn spec/01_book_spec.rb
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    self.condition = "new"
+  end
+
+end
+
+
+# learn spec/01_book_spec.rb
