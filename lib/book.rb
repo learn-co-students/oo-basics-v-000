ecfcd565e8581e5bd79f221929b033f6fc5c1135
Sofia Candiani <sofia.candiani123@gmail.com>
2017-09-06 21:27:12 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
-
-
+class Book
+  attr_accessor :author,:page_count,:genre
+  attr_reader :title
+  def initialize(title)
+    @title = title
+  end
+  #def title this is a getter method, we can use attr_reader instead
+    #@title
+  #end
+  def turn_page
+      puts "Flipping the page...wow, you read fast!"
+  end
+end
