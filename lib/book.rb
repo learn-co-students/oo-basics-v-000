b54a5488ab5669333940a856faf77ce12060852c
aisayo <aisayo@icloud.com>
2017-09-08 00:59:24 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
 
+  attr_accessor :author, :page_count, :genre #getter method:
+  attr_reader :title #setter 
 
+  def initialize(title)
+    @title = title
+  end
+
+   def turn_page
+     puts "Flipping the page...wow, you read fast!"
+   end
+
+end
