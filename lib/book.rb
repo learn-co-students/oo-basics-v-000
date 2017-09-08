35a686b61218ddb525b4fe19e3a6eea4e1dfc351
Johan Esteves <jsesteves91@gmail.com>
2017-09-08 00:47:03 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,41 @@
 
+class Book
 
+attr_accessor :title, :author, :page_count, :genre
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(num)
+    @page_count = num
+
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+
+  end
+
+end
