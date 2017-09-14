7e2b21253448e5cfa7a481018cec68ffa6b3ced4
Ally Kadel <akadel01@gmail.com>
2017-09-14 00:33:10 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+require 'pry'
+
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
 
 
+#binding.pry
