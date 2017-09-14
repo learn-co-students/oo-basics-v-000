96ae94060da6765fa3c16c610c88838b266a9b0f
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-09-14 19:53:00 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,10 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_writer :title
+  attr_accessor :title, :author, :page_count, :genre
 
   def initialize(title)
     @title = title
   end
+
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
