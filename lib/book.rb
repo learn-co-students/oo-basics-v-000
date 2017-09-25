e90ca8c93fb057164b2abab9bb03bd79d18084dd
Andrea Nasuto <andrea.nasuto@gmail.com>
2017-09-25 19:06:57 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,45 @@
+class Book
 
+def initialize(title_given)
+  @title = title_given
+end
 
+def title
+  @title
+end
+
+def author=(author_name)
+  @author = author_name
+end
+
+def author
+  @author
+
+end
+
+def page_count=(num)
+  @page_count = num
+  
+end
+
+def page_count
+  @page_count
+
+end
+
+def genre=(genre_given)
+  @genre = genre_given
+
+end
+
+def genre
+  @genre
+
+end
+
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+
+end
+
+end
