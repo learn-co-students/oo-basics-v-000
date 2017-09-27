84827bf35416db8a153dda24cb1117a64635f114
Brad Carlson <b2carlso@yahoo.com>
2017-09-27 00:30:58 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
 
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
+  def initialize(title)
+    @title = title ## creates an instance variable using @
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
