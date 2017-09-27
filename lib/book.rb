9376060d1ff996c51627b86491634259911c046c
Emily Morgan <emorgan05@gmail.com>
2017-09-27 14:51:25 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
+  def initialize(title)
+    @title = title
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
+name_of_the_wind = Book.new("The Name of the Wind")
+name_of_the_wind.author = "Patrick Rothfuss"
+puts name_of_the_wind.title
+puts name_of_the_wind.author
+name_of_the_wind.page_count = 800
+puts name_of_the_wind.page_count
+name_of_the_wind.genre = "fantasy"
+puts name_of_the_wind.genre
+name_of_the_wind.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,26 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
+
+saucony = Shoe.new("Saucony")
+saucony.color = "blue"
+saucony.size = 6
+saucony.material = "cloth with mesh around the toes"
+saucony.condition = "loved"
+puts saucony.brand
+puts saucony.color
+puts saucony.size
+puts saucony.material
+puts saucony.condition
+saucony.cobble
+puts saucony.condition
