1efd2f8fef3c04faa0ab1585f8cebb7249b56878
Connie Hyman <constance.hyman@mchsi.com>
2017-09-30 05:02:17 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,37 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+  def title
+    @title
+  end
 
+  def author=(name)
+    @name = name
+  end
+
+  def author
+    @name
+  end
+
+  def page_count=(number)
+    @number = number
+  end
+
+  def page_count
+    @number
+  end
+
+  def genre=(subject)
+    @subject = subject
+  end
+
+  def genre
+    @subject
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
