0f3b9a759381561c1285cbcb09550e982f761937
Rebecca Tong <rtong@buffalo.edu>
2017-10-30 20:24:24 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+
+class Shoe
+
+attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+def cobble
+  if @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
+
+
+end
