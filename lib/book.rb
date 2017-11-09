5248cc93ea4fb75957cb23ad7381f953ded0fc3d
charityyoungblood <youngblood.charity@gmail.com>
2017-11-09 15:09:21 -0800

    added attr_accessor for author, page_count, genre ; added comments for keeping code DRY

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,17 @@
 require 'pry'
 
 class Book 
+  attr_accessor :author, :page_count, :genre
+  
   def initialize(title)
     @title = title
   end
   def title 
     @title
   end 
+=begin
+
+**The code below is an example of non-DRY code, we replaced this code with the attribute accessor, which automatically sets up setter and getter methods for each method **
   # setter method here 
   def author=(author_name)
     @author = author_name
@@ -23,5 +28,18 @@ class Book
   def page_count
     @page_count
   end
+  # setter method
+  def genre=(genre_type)
+    @genre = genre_type
+  end
+  # getter method
+  def genre 
+    @genre
+  end
+=end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
 end 
 
