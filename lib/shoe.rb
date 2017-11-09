8c682e53bfcdf672a0b703d876e35c123856f835
charityyoungblood <youngblood.charity@gmail.com>
2017-11-09 15:53:10 -0800

    added getter and setter method for condition

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 # Make your shoe class here!
 class Shoe 
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :color, :size, :material
   attr_reader :brand
   
   def initialize(brand)
@@ -10,10 +10,16 @@ class Shoe
   # look at what methods require a setter and getter and instead of using multiple getters and setters, use attribute readers and attribute accessors 
   # :brand <getter only> , :color, :size, :material, :condition
   
+  def condition=(shoe_condition_now)
+    @shoe_condition = shoe_condition_now
+  end
+  def condition
+    @shoe_condition
+  end
   # create method cobble - outputs test string exactly 
   # this method also changes a shoes condition - needs access to the condition 
   def cobble
     puts "Your shoe is as good as new!"
-    shoe.condition = "new"
+    @shoe_condition = "new"
   end
 end 
