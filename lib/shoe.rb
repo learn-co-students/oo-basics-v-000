97499375c4325995a4e7c2ee2466124ada80b545
charityyoungblood <youngblood.charity@gmail.com>
2017-11-09 15:32:20 -0800

    added cobble method

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,18 @@
 # Make your shoe class here!
 class Shoe 
-  attr_accessor :brand, :color
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
   def initialize(brand)
     @brand = brand
   end
-  def brand
-    @brand
-  end
+
   # look at what methods require a setter and getter and instead of using multiple getters and setters, use attribute readers and attribute accessors 
-  # :brand , :color, :size, :material, :condition
+  # :brand <getter only> , :color, :size, :material, :condition
   
   # create method cobble - outputs test string exactly 
   # this method also changes a shoes condition - needs access to the condition 
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
 end 
