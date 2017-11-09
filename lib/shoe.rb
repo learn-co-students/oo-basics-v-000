86e16ce6695a8a0446fb703cace7cc2a5ddd13de
charityyoungblood <youngblood.charity@gmail.com>
2017-11-09 15:18:36 -0800

    added comments on how to approach problem

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,10 @@
 # Make your shoe class here!
+class Shoe 
+  def initialize(brand)
+    @brand = brand
+  end
+  def brand
+    @brand
+  end
+  # look at what methods require a setter and getter and instead of using multiple getters and setters, use attribute readers and attribute accessors 
+end 
