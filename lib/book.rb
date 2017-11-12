04762d489093436f77c1d767aa2ed29144535c87
Olena Ageyeva <eageeva@hotmail.com>
2017-11-12 20:51:47 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+  def initialize(title)
+    @title=title
+  end
+  attr_accessor :title, :author, :page_count, :genre
 
+  define_method("turn_page") do
+     #@page_count+=1
+     puts "Flipping the page...wow, you read fast!"
+  end
 
+end
