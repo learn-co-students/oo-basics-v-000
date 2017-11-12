485f99ce78686815113890f8d99008e6601c74f5
Olena Ageyeva <eageeva@hotmail.com>
2017-11-12 21:06:31 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,22 @@
 class Book
+
+  attr_accessor  :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES=[]
+
   def initialize(title)
     @title=title
   end
-  attr_accessor :title, :author, :page_count, :genre
 
-  define_method("turn_page") do
-     #@page_count+=1
+  def turn_page      #@page_count+=1
      puts "Flipping the page...wow, you read fast!"
   end
+
+  def genre=(genre)
+    @genre=genre
+    GENRES<<genre
+  end
+
 
 end
