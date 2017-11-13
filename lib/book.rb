19213b579a67163ffa52edad675cd7716d808042
Sherry <sherryptk@gmail.com>
2017-11-13 19:38:34 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+attr_accessor :author, :page_count, :genre, :turn_page
+  def initialize(title)
+    @title = title
+  end
 
+  def title
+      @title
+    end
 
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
+end
