d5527e7578cc19bf90ebf996a7cae431c62e794f
Nathan  Visak <nathan.visak@gmail.com>
2017-11-22 22:05:40 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+  def title=(title)
+    @title = title
+  end
 
+  def title
+    @title
+  end
+
+  attr_accessor :author, :page_count, :genre
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
