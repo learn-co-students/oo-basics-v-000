66ef7bad3d4030c987f7e3cd1c2c492b06e7cd09
Lekan Nicholson <lekan.nicholson@gmail.com>
2017-12-03 19:39:20 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition #creates the setters and getters for these
+  attr_reader :brand #sets the reader for this
+
+  def initialize(brand) #creates the setter for brand when class is created
+    @brand = brand
+  end
+
+  def cobble #this changes the shoes condidtion with self. (calls the class working on) to new and adds the string 
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
