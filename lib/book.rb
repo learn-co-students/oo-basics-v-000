20c1941c0c4e398f50cd1a5997c5195c4a66a956
Jonathan Luk <jluk87@gmail.com>
2017-12-09 22:34:15 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,8 @@
 class Book
   attr_accessor :author, :page_count, :genre
+  attr_reader :title
   def initialize(title)
     @title = (title)
-  end
-
-  def title
-    @title
   end
 
   def turn_page
