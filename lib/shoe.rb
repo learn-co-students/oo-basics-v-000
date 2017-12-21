89c5f6dfd17aa62cc18cd689a7ceaf6f2fde53af
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2017-12-21 13:06:51 +0000

    Lesson Solution

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,54 @@
-# Make your shoe class here!
+class Shoe
+  # initialization writer
+  def initialize(brand)
+    @brand = brand
+  end
+  # initialization reader
+  def brand
+    @brand
+  end
+  
+  # color writer
+  def color=(color)
+    @color = color
+  end
+  # color reader
+  def color
+    @color
+  end
+  
+  # size writer
+  def size=(size)
+    @size = size
+  end
+  # size reader
+  def size
+    @size
+  end
+  
+  # material writer
+  def material=(material)
+    @material = material
+  end
+  # material reader
+  def material
+    @material
+  end
+  
+  # condition writer
+  def condition=(condition)
+    @condition = condition
+  end
+  # condition reader
+  def condition
+    @condition
+  end
+  
+  # cobble
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+  
+end
+
