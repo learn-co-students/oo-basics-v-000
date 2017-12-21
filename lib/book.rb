701903cba740a34fea217fad48cc8a529604007d
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2017-12-21 11:50:52 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,44 @@
-
+class Book
+  # INITIALIZATION WRITER
+  def initialize(title)
+    @title = title
+  end
+  # INITIALIZATION READER
+  def title
+    @title
+  end
+  
+  # AUTHOR WRITER
+  def author=(author)
+    @author = author
+  end
+  # AUTHOR READER
+  def author
+    @author
+  end
+  
+  # COUNT WRITER
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+  # COUNT READER
+  def page_count
+    @page_count
+  end
+  
+  # GENRE WRITER
+  def genre=(genre)
+    @genre = genre
+  end
+  # GENRE READER
+  def genre
+    @genre
+  end
+  
+  # TURN PAGE
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
 
