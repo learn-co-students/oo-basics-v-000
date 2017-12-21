c61a9ccbe42149fa855619de3d5aec5a8dab09eb
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2017-12-21 13:10:22 +0000

    Refactored Solution

--- lib/book.rb
+++ lib/book.rb
@@ -1,44 +1,14 @@
 class Book
-  # INITIALIZATION WRITER
+  # Attributes
+  attr_accessor :title, :author, :page_count, :genre 
+  
+  # Initialiazation Writer
   def initialize(title)
     @title = title
   end
-  # INITIALIZATION READER
-  def title
-    @title
-  end
   
-  # AUTHOR WRITER
-  def author=(author)
-    @author = author
-  end
-  # AUTHOR READER
-  def author
-    @author
-  end
-  
-  # COUNT WRITER
-  def page_count=(page_count)
-    @page_count = page_count
-  end
-  # COUNT READER
-  def page_count
-    @page_count
-  end
-  
-  # GENRE WRITER
-  def genre=(genre)
-    @genre = genre
-  end
-  # GENRE READER
-  def genre
-    @genre
-  end
-  
-  # TURN PAGE
+  # Turn Page
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  
 end
-
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,47 +1,10 @@
 class Shoe
-  # initialization writer
+  # Attributes
+  attr_accessor :brand, :color, :size, :material, :condition 
+  
+  # Initialiazation Writer
   def initialize(brand)
     @brand = brand
-  end
-  # initialization reader
-  def brand
-    @brand
-  end
-  
-  # color writer
-  def color=(color)
-    @color = color
-  end
-  # color reader
-  def color
-    @color
-  end
-  
-  # size writer
-  def size=(size)
-    @size = size
-  end
-  # size reader
-  def size
-    @size
-  end
-  
-  # material writer
-  def material=(material)
-    @material = material
-  end
-  # material reader
-  def material
-    @material
-  end
-  
-  # condition writer
-  def condition=(condition)
-    @condition = condition
-  end
-  # condition reader
-  def condition
-    @condition
   end
   
   # cobble
@@ -49,6 +12,13 @@ class Shoe
     puts "Your shoe is as good as new!"
     @condition = "new"
   end
-  
 end
+
+
+
+
+
+  
+
+  
 
