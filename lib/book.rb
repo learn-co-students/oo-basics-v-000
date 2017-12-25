4bd6ad8a7813c4c0b3bcd608c948f59a76ff4bed
jtorre20 <jtorres1989@gmail.com>
2017-12-25 16:51:14 -0500

    used self, done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,45 @@
+class Book
+  
+  def initialize(title)
+    @title = title
+  end
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
+=begin
+  def title
+    @title
+  end
+
+  
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(page)
+    @page_count = page
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+=end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
