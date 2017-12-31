59a0e63dd2d67a308d4fff2bd9bf69b6fd957dea
Joey Cho <cjy12182000@naver.com>
2017-12-31 02:14:05 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+class Book
 
+  def initialize(title)
+    @temp = title
+  end
 
+  def title
+    return @temp
+  end
+
+  attr_accessor :author
+  attr_accessor :page_count
+  attr_accessor :genre
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
