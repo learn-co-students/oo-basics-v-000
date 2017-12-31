2a698b2c838aace0f01fb59970de6efbcd897bad
Laura <laurao148@gmail.com>
2017-12-31 21:45:48 +0000

    Done.

---
+++ spec.rb
@@ -0,0 +1,29 @@
+
+An error occurred while loading ./spec/02_shoe.
+Failure/Error: load file
+
+LoadError:
+  cannot load such file -- /home/laurao148-101279/code/labs/oo-basics-v-000/spec/02_shoe
+# /usr/local/rvm/gems/ruby-2.3.1/gems/rspec-core-3.7.0/lib/rspec/core/configuration.rb:1924:in `load'
+# /usr/local/rvm/gems/ruby-2.3.1/gems/rspec-core-3.7.0/lib/rspec/core/configuration.rb:1924:in `load_spec_file_handling_errors'
+# /usr/local/rvm/gems/ruby-2.3.1/gems/rspec-core-3.7.0/lib/rspec/core/configuration.rb:1496:in `block in load_spec_files'
+# /usr/local/rvm/gems/ruby-2.3.1/gems/rspec-core-3.7.0/lib/rspec/core/configuration.rb:1494:in `each'
+# /usr/local/rvm/gems/ruby-2.3.1/gems/rspec-core-3.7.0/lib/rspec/core/configuration.rb:1494:in `load_spec_files'
+# /usr/local/rvm/gems/ruby-2.3.1/gems/rspec-core-3.7.0/lib/rspec/core/runner.rb:100:in `setup'
+# /usr/local/rvm/gems/ruby-2.3.1/gems/rspec-core-3.7.0/lib/rspec/core/runner.rb:86:in `run'
+# /usr/local/rvm/gems/ruby-2.3.1/gems/rspec-core-3.7.0/lib/rspec/core/runner.rb:71:in `run'
+# /usr/local/rvm/gems/ruby-2.3.1/gems/rspec-core-3.7.0/lib/rspec/core/runner.rb:45:in `invoke'
+# /usr/local/rvm/gems/ruby-2.3.1/gems/rspec-core-3.7.0/exe/rspec:4:in `<top (required)>'
+# /usr/local/rvm/gems/ruby-2.3.1/bin/rspec:23:in `load'
+# /usr/local/rvm/gems/ruby-2.3.1/bin/rspec:23:in `<main>'
+# /usr/local/rvm/gems/ruby-2.3.1/bin/ruby_executable_hooks:15:in `eval'
+# /usr/local/rvm/gems/ruby-2.3.1/bin/ruby_executable_hooks:15:in `<main>'
+# 
+#   Showing full backtrace because every line was filtered out.
+#   See docs for RSpec::Configuration#backtrace_exclusion_patterns and
+#   RSpec::Configuration#backtrace_inclusion_patterns for more information.
+No examples found.
+
+Finished in 0.00032 seconds (files took 0.48032 seconds to load)
+0 examples, 0 failures, 1 error occurred outside of examples
+
