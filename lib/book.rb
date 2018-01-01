833a06cb4e53a9b997ca48d34e7452195cd07534
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-01 21:00:51 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,37 @@
+class Book
+  def initialize(name = "And Then There Were None", author)
+     @title = name
+  end
 
+  def title
+    @title
+  end
 
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(page)
+    @page_count = page
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
