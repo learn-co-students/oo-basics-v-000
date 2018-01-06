cecf3087eba1599e2a5c693bc85800d7c52dd073
EPIC448 <samrey2018@gmail.com>
2018-01-06 04:03:43 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
 # Make your shoe class here!
+# for an questions look back at your book.rb code in the same folder
+class Shoe
+# note By using attr_accessor :brand, :color, :confounded:ize, :material, :condition at the top of the code, you have essentially created variables that can now be accessed using the @ symbol
+#example on line 16
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+    puts "Your shoe is as good as new!"
+        @condition = 'new'
+    end
+
+end
