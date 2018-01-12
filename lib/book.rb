6d0c0c2ea0f5a095c06c30f58780f2d28608d301
Allyson Wesman <allysonwesman@gmail.com>
2018-01-11 22:01:09 -0800

    solution

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
 
+  def initialize(title)
+    @title = title
+  end
 
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
