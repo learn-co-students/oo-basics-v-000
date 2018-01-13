ecf9f25ff18ded21cd0ea8544c0acec0ce1bb559
aviflombaum <avi@flombaum.com>
2018-01-13 14:26:26 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,24 @@
 class Book
+  attr_accessor :page_count, :genre
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
 end
