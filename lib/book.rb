15d0e3658cc0a3483e7698ea94cfc6e93734b56d
Alli Tzadok <allisontzadok@gmail.com>
2018-01-20 22:10:55 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,9 @@
 #learn spec/01_book_spec.rb
 class Book
   attr_accessor :author, :page_count, :genre
+  attr_reader :title
   def initialize(title)
     @title = title
-  end
-
-  def title
-    @title
   end
 
   def turn_page
