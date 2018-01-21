2ca9d210cef9677c55f43df94f0c12777de7de11
Kriti Rai <kriti.rai@gmail.com>
2018-01-21 23:33:26 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,6 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition
+  attr_accessor :brand
   def initialize(brand)
     @brand = brand
   end
