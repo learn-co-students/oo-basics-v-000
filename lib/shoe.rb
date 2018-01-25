f64ce11dc9800cff64a210baeec9036da5560aae
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-01-25 19:55:38 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,8 +5,11 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
+<<<<<<< HEAD
   def cobble
     puts "Your shoe is as good as new!"
     @condition = "new"
   end
+=======
+>>>>>>> 8f003167acf85ca87379fb3ec1929189acfcb5e9
 end
