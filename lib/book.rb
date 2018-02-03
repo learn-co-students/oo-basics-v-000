48dcc421a37c75b88629f97ad0a0096b3ac0fadd
Sean Oughton <seanoughton@gmail.com>
2018-02-03 00:56:06 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+  attr_accessor :author, :page_count, :genre
 
+  def initialize(title)
+    @title = title
+  end
 
+  attr_reader :title
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+
+Book.new("And Then There Were None")
+book = Book.new("Some Title")
+book.turn_page
