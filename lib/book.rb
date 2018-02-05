410053980d8f3e75930961e83e072cab2e13406d
nptravis <nptravis@yahoo.com>
2018-02-05 12:10:18 +0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,33 @@
+class Book
+
+	attr_accessor :author, :page_count, :genre
+	attr_reader :title
+
+	def initialize(title)
+		@title = title
+	end
+
+	def turn_page
+		puts "Flipping the page...wow, you read fast!"
+	end
+
+end
+
+class Shoe
+
+	attr_accessor :color, :size, :material, :condition
+	attr_reader :brand
+
+	def initialize(brand)
+		@brand = brand
+	end
+
+	def cobble
+		self.condition = "new"
+		puts "Your shoe is as good as new!"
+	end
+
+end
+
 
 
