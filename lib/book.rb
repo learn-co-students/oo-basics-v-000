bd2291e4fa258b4df26d9d49e4d4504559ee7682
Marin Cabac <marin.cabac@gmail.com>
2018-02-18 23:39:41 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
 
 
+class Book
+
+attr_accessor :author, :page_count, :genre
+
+  def initialize(title)
+    @title=title
+  end
+
+   attr_reader :title
+
+  def turn_page
+
+puts "Flipping the page...wow, you read fast!"
+
+  end
+
+end
