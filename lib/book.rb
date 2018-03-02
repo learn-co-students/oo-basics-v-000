25bb46b01f100e78356789ae09e41a514510a65e
Brandon Chin <brandonchin@me.com>
2018-03-02 04:03:57 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,17 +1,12 @@
 class Book
+  attr_accessor :page_count, :genre, :author
+  attr_reader :title
+
   def initialize(title)
     @title = title
   end
 
-  def title
-    @title
-  end
-
-  def author=(author)
-    @author = author
-  end
-
-  def author
-    @author
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
 end
