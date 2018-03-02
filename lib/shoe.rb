f79ac4a307e27e926450ef1586a4589e62e6c05d
Katie Cassell <kcassell944@gmail.com>
2018-03-02 22:15:17 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,28 @@
-# Make your shoe class here!
+class Shoe
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+
+def brand
+  @brand
+end
+
+def color=(color)
+  @color = color
+end
+
+def size=
+  @size = size
+end
+
+def material=
+  @material = material
+end
+
+def condition=
+  @condition = condition
+end
+
+end
