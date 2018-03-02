e66be831dfe6c3d26d993a2e3d6c04d6529aa48e
Brandon Chin <brandonchin@me.com>
2018-03-02 02:24:03 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+  def title
+    @title
+  end
 
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+end
