7d2febe10c413ed65b853c10f088ffc027176ed5
Beth Schofield <bethmschofield@gmail.com>
2018-03-03 06:00:02 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre
+  def initialize(title)
+    @title = title
+    @current_page = 0
+  end
 
-
+  def turn_page
+    @current_page += 1
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
