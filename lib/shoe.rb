764e4c796de311da43e8963154a4401d101e1684
Taylor Donlan <taylordonlan@gmail.com>
2018-03-07 20:44:01 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 
 class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition
+  attr_accessor  :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
