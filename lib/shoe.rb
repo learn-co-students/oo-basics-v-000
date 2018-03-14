367c9da396e77939a4eea095187b7f43f017efe9
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-14 14:38:14 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
+class Shoe
+  
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :title
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
+
+book = Book.new("Some Title")
+book.turn_page
+
+
 # Make your shoe class here!
