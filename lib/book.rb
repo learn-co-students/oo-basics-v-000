062e93fe7d83052bbf909401237f0ef7278070e5
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-03-20 15:48:37 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,7 @@
 class Book
   
-  attr_accessor :title, :author, :page_count, :genre
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
   
   def initialize(title)
     @title = title
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,7 @@
 class Shoe
   
-  attr_accessor :brand, :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   
   def initialize(brand)
     @brand = brand
