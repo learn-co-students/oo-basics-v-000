929391263773bf270798e9549b52579ea8753e3d
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-03-20 15:43:07 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,17 +1,13 @@
 class Book
-
-  attr_accessor :author, :page_count, :genre
-
+  
+  attr_accessor :title, :author, :page_count, :genre
+  
   def initialize(title)
     @title = title
   end
-
-  def title
-    @title
-  end
-
+  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
+  
 end
