926e0e115e495af93df8e6c41513797f1c4fd17e
Kaushal Patel <kaushalpatel778@gmail.com>
2018-04-07 03:23:28 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,59 @@
+class Book
 
 
+      def initialize(title)
+        @title = title
+      end
+      def title
+        @title
+      end
+
+      def author
+        @author
+      end
+      def author= (author)
+        @author = author
+      end
+
+      def page_count=(num)
+        @page_count = num
+      end
+      def page_count
+        @page_count
+      end
+
+      def genre=(genre)
+          @genre = genre
+        end
+      def genre
+        @genre
+      end
+
+      def turn_page
+         puts "Flipping the page...wow, you read fast!"
+      end
+end
+
+book = Book.new("Some Title")
+book.turn_page
+
+=begin
+# book.rb
+# USING ATTRIBUTE ACCESSORS AND ATTRIBUTE READERS
+class Book
+  attr_accessor :author, :page_count, :genre
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+=end
