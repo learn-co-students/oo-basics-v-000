4e1ee6fe8146d664d06f646b46be32a188bdcaa2
Sean Febo <seanfebo@techie.com>
2018-04-10 20:16:13 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
-
-
+class Book 
+  def initialize(title)
+    @title = title
+  end
+  def title
+    @title
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  attr_accessor :author, :page_count, :genre
+  end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class Shoe 
+  def initialize(brand)
+    @brand = brand
+  end 
+  def brand
+    @brand
+  end
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+   attr_accessor :color, :size, :material, :condition
+end
+  
+
