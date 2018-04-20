01549f09f1457d95d9d6bed25975a2c4a4697bf4
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-20 05:50:59 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
 
+  def initialize(title)
+    @title = title
+  end
 
+  attr_accessor :author,:page_count,:genre #getter/setter for mulitple attributes
+  attr_reader :title #getter getting title
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
