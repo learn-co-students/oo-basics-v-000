1bc68a7fad6b930ee761cbfda725f3e6b75f9e92
Jamal Gelin <gelin.jamal@gmail.com>
2018-05-01 16:41:20 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
 # Make your shoe class here!
+class Book
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+end
