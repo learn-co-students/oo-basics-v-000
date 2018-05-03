51b1b80a279e23e37cbf98b0e28473b5196455d1
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-03 01:06:21 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,31 @@
+class Book
+  attr_accessor   :author, :page_count, :genre
+  attr_reader   :title
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
 
+first_book = Book.new("The last Jedi")
+
+puts first_book
+
+puts first_book.title
+
+puts first_book.author = "Toni Olukunle"
+
+puts first_book.page_count = 200
+
+puts first_book.turn_page
+
+puts first_book.genre = "Sci Fi"
+
+puts second_book = Book.new("Jumanji")
+
+puts second_book.title
