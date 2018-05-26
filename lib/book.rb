c22d6a5cd7c9169d832ba699703a2e0195d9c4d4
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-26 19:26:06 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+attr_accessor :genre, :page_count
 
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
