cb5a61aea4fffc6be42ff6e64274eaf01b0aae83
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-08 18:47:12 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+  attr_accessor :author, :genre, :page_count
+  attr_reader :title
 
+  def initialize(title)
+    @title = title
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!" 
+  end
+end
 
+my_book = Book.new("Ender's Game")
+my_book.turn_page
