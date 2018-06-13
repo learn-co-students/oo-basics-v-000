57752e32c88d7795b6caec8f03625215abf3a217
Brandon Green <brandon@brandonleeproductions.com>
2018-06-13 03:19:16 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,41 @@
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
+  def initialize(title)
+    @title = title
+  end
+
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
+
+#  def author=(author) #setter for author
+#    @author = author
+#  end
+#  
+#  def author #getter for author 
+#    @author
+#  end
+#  
+#  def page_count=(num)
+#    @page_count = num
+#  end
+#  
+#  def page_count
+#    @page_count
+#  end
+#
+# def genre=(genre)
+#    @genre = genre
+#  end
+#  
+#  def genre
+#    @genre
+#  end
+#  
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+  # should brand be an attr_reader?
+  
+ def cobble
+   puts "Your shoe is as good as new!"
+   @condition = "new"
+ end
+  
+end
