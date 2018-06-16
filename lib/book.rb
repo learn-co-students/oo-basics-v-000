67213568e58013753a326cc2fedb9adae18832ef
Gabriel Valle <gvalle91@gmail.com>
2018-06-16 17:02:08 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
-
-
+class Book
+  attr_accessor :author, :page_count, :genre
+  #attr_writer 
+  attr_reader :title
+  
+  def initialize(title)
+    @title = title
+    @current_page = 0
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+    @current_page += 1
+  end
+  
+end
