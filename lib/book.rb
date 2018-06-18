cb4be9b95c8dda1c6f0b9bf11e412c644aaeee56
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-18 18:26:18 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
-
-
+class Book
+  attr_accessor :author,:genre,:page_count
+def initialize(title)
+  @title = title
+end
+def title=(title_name)
+  @title = title
+end
+def title
+  @title
+end
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+end
