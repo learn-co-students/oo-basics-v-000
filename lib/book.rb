fbbda967db24761bf76b36709a2c73e646fcee26
Priscilla H <priscillamhuynh@gmail.com>
2018-06-28 10:27:46 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,41 @@
+class Book
+  def initialize(title)
+    @title=title
+  end
 
+  def title
+    @title
+  end
 
+  def author=(author)
+    @author=author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(num)
+    @page_count=num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(type)
+    @genre=type
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+  def condition
+    condition == "new"
+  end
+end
