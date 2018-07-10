bcfe48feaf58c52c3bc014f8594048e56ee63c7f
Jonathan Hong <jonhong777@gmail.com>
2018-07-10 15:52:40 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,38 @@
-
+class Book
+  def initialize(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  def author=(new_author)
+    @author = new_author
+  end
+  
+  def author 
+    @author
+  end
+  
+  def page_count=(pages)
+    @page_count = pages
+  end 
+  
+  def page_count 
+    @page_count
+  end
+  
+  def genre=(new_genre)
+    @genre = new_genre
+  end
+  
+  def genre 
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
