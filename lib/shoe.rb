8c5a1024fd3a4756f94fa14f1273bc319c798599
Daniel Etter <etter.dt@gmail.com>
2018-07-14 21:11:00 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,17 +1,9 @@
 class Shoe
-  attr_accessor :color, :size, :material
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
   def initialize(brand)
     @brand = brand
-  end
-  
-  def condition=(condition)
-    @condition = condition
-  end
-  
-  def condition
-    @condition
   end
   
   def cobble
