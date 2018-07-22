93fe5e7fc8b61b8acba6f53b1ec88bb6d98c493d
Anna Dybas <a.m.dybas@gmail.com>
2018-07-22 00:16:17 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,15 @@
 class Book
 
   attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
   def initialize(title)
     @title = title
   end
 
-  def title
-    @title
-  end
+  # def title
+  #   @title
+  # end
 
   # def author= (author)
   #   @author = author
