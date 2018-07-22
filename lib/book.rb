e28465043e71b76a1a38d2e760acfe263af97f79
Anna Dybas <a.m.dybas@gmail.com>
2018-07-22 00:15:25 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,7 @@
 class Book
 
+  attr_accessor :author, :page_count, :genre
+
   def initialize(title)
     @title = title
   end
@@ -8,29 +10,29 @@ class Book
     @title
   end
 
-  def author= (author)
-    @author = author
-  end
+  # def author= (author)
+  #   @author = author
+  # end
+  #
+  # def author
+  #   @author
+  # end
 
-  def author
-    @author
-  end
+  # def page_count= (num)
+  #   @page_count = num
+  # end
+  #
+  # def page_count
+  #   @page_count
+  # end
 
-  def page_count= (num)
-    @page_count = num
-  end
-
-  def page_count
-    @page_count
-  end
-
-  def genre= (genre)
-    @genre = genre
-  end
-
-  def genre
-    @genre
-  end
+  # def genre= (genre)
+  #   @genre = genre
+  # end
+  #
+  # def genre
+  #   @genre
+  # end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
