0ee8c12e6d15e9bea5538eb19c59c4c6f41fa2cd
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-08-03 00:45:52 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,21 +1,13 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :material
+  attr_accessor :brand, :color, :size, :material, :condition
   
   def initialize(brand)
     @brand = brand
   end 
  
-  def condition=(condition)
-    @condition = condition
-  end
-  
-  def condition
-    @condition
-  end
-
   def cobble
-        puts "Your shoe is as good as new!"
-        @condition=("new")
-    end
+     puts "Your shoe is as good as new!"
+     @condition=("new")
+  end
   
 end
