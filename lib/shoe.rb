2d335ed8d5fd9528195270900ec8b1b1b406d20f
Rafael Lopez <lpz.raf@gmail.com>
2018-08-05 01:20:20 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
 # Make your shoe class here!
+class Shoe 
+  attr_accessor :brand, :color, :size, :material, :condition
+  def initialize(shoe)
+    @shoe = shoe
+  end
+  
+  def cobble
+  
+  end
+end
