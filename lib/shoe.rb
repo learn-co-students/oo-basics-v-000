8e4a14fccce37c5d9175cb0f9e61709dd1ba3bd7
Steven Crouse <sdcrouse91@gmail.com>
2018-08-28 01:35:35 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,28 @@
 # Make your shoe class here!
+
+class Shoe 
+  attr_accessor :color, :size, :material
+  attr_reader :brand 
+  
+  def initialize(brand)
+    @brand = brand 
+  end
+  
+  # Note: to make #cobble more understandable, I intentionally defined #condition=
+  # and #condition, rather than wrap them under attr_accessor.
+  
+  def condition=(condition)
+    @condition = condition
+  end 
+  
+  def condition
+    @condition 
+  end
+  
+  def cobble 
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+    # Note: self.condition = "new" works as well
+  end
+  
+end
