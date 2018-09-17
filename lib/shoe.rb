75e447355bdfa88b807491594a82943490ecc13c
jontenthoff <jontenthoff@hotmail.com>
2018-09-17 20:05:36 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,10 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  def initialize(brand)
+    @brand = brand
+  end
+  def cobble()
+    "The show has been repaired."
+  end
+  
