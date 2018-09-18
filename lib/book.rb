2406751d8ce9cc8d0649d2c0f83d5d53e128c73b
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-09-18 10:28:05 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,43 @@
 
 
+class Book 
+  
+  def initialize(title) 
+    @title = title 
+  end 
+
+  def title
+    @title 
+  end 
+  
+  def author= (author)
+    @author = author 
+  end 
+  
+  def author
+    @author
+  end 
+  
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
+  
+   def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end 
+
+
