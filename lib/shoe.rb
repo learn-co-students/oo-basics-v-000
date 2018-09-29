253b7d2835a1442cf8032ac28c06c5e1f988d9ae
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-29 21:07:43 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -12,19 +12,3 @@ class Shoe
   end 
   
 end
-
-
-
-# class Book
-#   attr_accessor :author, :page_count, :genre
-#   attr_reader :title
-  
-#   def initialize(title)
-#     @title = title
-#   end 
-  
-#   def turn_page
-#     puts "Flipping the page...wow, you read fast!"
-#   end 
-  
-# end 
