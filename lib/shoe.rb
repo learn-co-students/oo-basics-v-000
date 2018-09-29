235aa9d4cacb9c30f205ef51fb3a8212113e9fb9
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-29 21:07:26 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,30 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand 
+  end 
+  
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end 
+  
+end
+
+
+
+# class Book
+#   attr_accessor :author, :page_count, :genre
+#   attr_reader :title
+  
+#   def initialize(title)
+#     @title = title
+#   end 
+  
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end 
+  
+# end 
