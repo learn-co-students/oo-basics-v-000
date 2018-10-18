dead89d3fe7b94ef43f5f5c1786d70e644dbac29
Paul Fell-Cinoman <swellpf@gmail.com>
2018-10-18 23:41:46 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+  
+  
+end
