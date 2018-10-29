8b901db73e9faa1d8206bb308292d8e9a7932c0b
Andrew Trent <andrewtrentdesign@gmail.com>
2018-10-29 21:38:46 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,9 @@
-
-
+class Book
+  attr_accessor :title, :author, :page_count, :genre
+  def initialize(title)
+    @title = title
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
