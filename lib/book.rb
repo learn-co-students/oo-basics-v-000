885874e0f078f3931af3b2f57a726b2e0e4f4c96
Lois Lee <lois.lee@me.com>
2018-11-06 20:09:29 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,14 @@
+class Book
 
-  class Book
-    attr_accessor :author, :page_count, :genre
-    attr_reader :title
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
-    def initialize(title)
-      @title = title
-    end
-
-    def turn_page
-      puts "Flipping the page...wow, you read fast!"
-    end
-
+  def initialize(title)
+    @title = title
   end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
