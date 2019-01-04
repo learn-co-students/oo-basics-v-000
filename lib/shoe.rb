47baab457d3e27dd51aa911731166e695e92836b
Sarah Paz <sarah.paz@gmail.com>
2019-01-04 01:29:59 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,9 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
   def initialize(brand)
     @brand = brand
-  end
-
-  def brand
-    @brand
   end
 
   def cobble
