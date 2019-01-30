842ab8ee9c013bb5b0de377907b7600c2dc5d47a
github username <github email address>
2019-01-30 12:02:00 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,35 +1,78 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
-  
-  def initialize(title)
-    @title = title
-  end
-  def title
-    @title
-  end
-  def author=(author) #setter
-    @author = author
-  end
-  def author #getter
-    @author
-  end
-  def page_count=(num)
-    @page_count = num
-  end
-  def page_count
-    @page_count
-  end
-  def genre=(genre)
-    @genre = genre
-  end
-  def genre
-    @genre
-  end
-  def turn_page #behavor method
-    puts "Flipping the page...wow, you read fast!"
-  end
+
+attr_accessor :author, :page_count, :genre, :turn_page
+attr_reader :title
+
+def initialize(title)
+  @title = title
 end
 
-book=Book.new("blank")
-book.turn_page
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Book
+#   attr_accessor :author, :page_count, :genre
+#   attr_reader :title
+#
+#   def initialize(title)
+#     @title = title
+#   end
+#   def title
+#     @title
+#   end
+#   def author=(author) #setter
+#     @author = author
+#   end
+#   def author #getter
+#     @author
+#   end
+#   def page_count=(num)
+#     @page_count = num
+#   end
+#   def page_count
+#     @page_count
+#   end
+#   def genre=(genre)
+#     @genre = genre
+#   end
+#   def genre
+#     @genre
+#   end
+#   def turn_page #behavor method
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+# end
+#
+# book=Book.new("blank")
+# book.turn_page
