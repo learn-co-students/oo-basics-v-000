4399ad3f4cf305fd199e1a3f67a4988855992ee5
Gigi M <mmgamao1@gmail.com>
2019-03-18 18:42:57 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,55 @@
 
+class Book
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+ def title
+   @title 
+   
+ end
+ 
+ def author=(author) 
+   @author = author 
+ end
+ 
+ def author  
+   @author
+ end 
+ 
+ def page_count=(page_count)
+   @page_count = page_count
+ end
+ 
+ def page_count
+   @page_count
+ end
+ 
+ def genre=(genre)
+   @genre = genre
+ end
+ def genre
+   @genre 
+ end
 
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+
+ end  
+ 
+ 
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
