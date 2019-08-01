cdd3ea74b8ed8ac5693911cd2c6078b906302cc8
Olivia Kesler <oakesler@protonmail.ch>
2019-08-01 19:26:09 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,35 @@
+class Book 
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
+  
+  #def page_count=(num)
+    #@page_count = num
+  #end
+  
+  #def page_count
+    #@page_count
+  #end
+  
+  #def genre=(genre)
+    #@genre = genre
+  #end
+  
+  #def genre
+    #@genre
+  #end
+  
+  #def turn page
+    #puts "Flipping the page...wow, you read fast!"
+  #end
+#end
 
