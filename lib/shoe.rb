b7bc8479e75c4654df486b734518df96b2224325
Julie  McManus <julieannesays@gmail.com>
2020-01-31 00:30:01 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,22 +1,13 @@
-class Shoe 
-	attr_accessor :color, :size, :material
-	attr_reader :brand
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
-	def initialize(brand)
-		@brand = brand
-	end
+  def initialize(brand)
+    @brand = brand
+  end
 
-	def condition=(condition)
-		@condition = condition 
-	end
-
-	def condition 
-		@condition 
-	end
-
-	def cobble 
-		@condition = "new"
-		puts "Your shoe is as good as new!"
-	end
-
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
 end
