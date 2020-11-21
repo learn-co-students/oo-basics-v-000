98cd8e8d9cc6f73be264d18dd6f55fde0644a906
andy-aguilar <aaguil3@gmail.com>
2020-11-21 05:43:03 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
