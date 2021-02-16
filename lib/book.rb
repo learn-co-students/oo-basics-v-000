18bf0b4c5bac0b2c0e92b07b555ca3ddb481e7e3
Kayla Cross <kac288@georgetown.edu>
2021-02-16 02:14:04 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,24 +1,13 @@
 class Book
-  def initialize(title)
-    @title = title
-  end
 
   attr_accessor :title, :author, :page_count, :genre
+
+  def initialize(title)
+    self.title = title
+  end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-end
 
-class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition
-
-  def initialize(brand)
-    @brand = brand
-  end
-
-  def cobble
-    puts "Your shoe is as good as new!"
-    @condition = "new"
-  end
 end
