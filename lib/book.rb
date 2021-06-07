46083364cda3fea8e1a945690b8a7aa816ece1af
Wen Yu <wyu10amy@hotmail.com>
2021-06-07 18:53:58 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book
+  def initialize(title="And Then There Were None")
+    @title = title
+  end
 
+  def title
+    @title
+  end
 
+  attr_accessor :author
+
+  attr_accessor :page_count
+
+  attr_accessor :genre
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
